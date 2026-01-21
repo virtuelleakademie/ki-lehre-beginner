e65b5eb671f321ab47efe611182f6b51302d33eb
Andrew Ellis <andrewellis@pm.me>
2026-01-22 00:45:13 +0100

    Refactor timer extension to encapsulate dependency management and improve code structure

--- _extensions/produnis/timer/timer.lua
+++ _extensions/produnis/timer/timer.lua
@@ -1,31 +1,37 @@
-quarto.doc.add_html_dependency({
-  name = "quarto-timer",
-  version = "1.0.0",
-  scripts = {"timer.js"},
-  stylesheets = {"timer.css"}
-})
+local counter = 0
+
+local function ensureDependency()
+  quarto.doc.add_html_dependency({
+    name = "quarto-timer",
+    version = "1.0.0",
+    scripts = {"timer.js"},
+    stylesheets = {"timer.css"}
+  })
+end
 
 return {
-    -- Füge den Filter zu Pandoc hinzu
-    {
-        Div = function (div)
-            if div.classes[1] == "timer" then
-                local containerId = div.identifier
-                local timeLimit = tonumber(div.attributes["seconds"]) or 300  -- Default: 300 Sekunden
-                local startOn = div.attributes["starton"] or "slide" -- Default: timers run when visible
-            
-                local htmlSnippet = string.format([[
-                    <div id="%s"></div>
-                    <script>
-                        document.addEventListener("DOMContentLoaded", function () {
-                            initializeTimer("%s", %d, "%s");
-                        });
-                    </script>
-                ]], containerId, containerId, timeLimit, startOn)
+  ['timer'] = function(args, kwargs, meta)
+    if not quarto.doc.isFormat("html:js") then
+      return pandoc.Null()
+    end
 
-                return pandoc.RawBlock("html", htmlSnippet)
-            end
-            return div
-        end
-    }
+    ensureDependency()
+
+    counter = counter + 1
+    local containerId = "timer-" .. counter
+    local minutes = tonumber(pandoc.utils.stringify(args[1])) or 5
+    local timeLimit = minutes * 60
+    local startOn = "slide"
+
+    local htmlSnippet = string.format([[
+<div id="%s"></div>
+<script>
+document.addEventListener("DOMContentLoaded", function () {
+    initializeTimer("%s", %d, "%s");
+});
+</script>
+]], containerId, containerId, timeLimit, startOn)
+
+    return pandoc.RawBlock("html", htmlSnippet)
+  end
 }
